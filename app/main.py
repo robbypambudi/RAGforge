bd0ec35e97a61f77e7ff6e2692cfd2d304f848de
reivertt <billthehuman27@gmail.com>
2025-10-18 19:17:21 +0700

    revert backend cors env

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ class App:
         if settings.BACKEND_CORS_ORIGINS:
             self.app.add_middleware(
                 CORSMiddleware,
-                allow_origins=settings.BACKEND_CORS_ORIGINS,
+                allow_origins=["http://localhost:8501", "http://localhost:3000", "http://10.21.79.95:3000"],
                 allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"],
